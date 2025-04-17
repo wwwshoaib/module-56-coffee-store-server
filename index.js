8a3b19d24a4813a9fd49c920f5dd180582e52526
Shoaib <shoaibchemistrysust@gmail.com>
2025-04-17 19:04:04 +0600

    data was sent to the server side

--- index.js
+++ index.js
@@ -32,12 +32,17 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    //send data to server
+    app.post('/addcoffee', async(req, res) => {
+      const newCoffee = req.body;
+      console.log(newCoffee);
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    await client.close(); 
   }
 }
 run().catch(console.dir);
